c751a9769e00308e610a1e0db6a3a0aa53cfed92
sherlock19 <holmes81@gmail.com>
2018-05-12 04:59:41 +0200

    Added test codes Set character encoding

--- portal-api/src/main/java/com/jade/portal/controller/TestController.java
+++ portal-api/src/main/java/com/jade/portal/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.jade.portal.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +17,22 @@ public class TestController {
 		return "getTest";
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.POST)
-	public String test (@PathVariable("id") String id,
-			@RequestParam(value="name", required =false, defaultValue="World") String n) {
-		return id+n;
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public String test(@PathVariable("id") String id,
+			@RequestParam(value="name", required =false, defaultValue="World") String name) {
+		return id+name;
 	}
-
+	
+	
+	@RequestMapping(value="/", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
+	public String test (@RequestBody String jsonStr  ) {
+		return jsonStr;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
+	public String test (@PathVariable("id") String id,
+			@RequestParam(value="name", required=false, defaultValue="World") String name,  
+			@RequestParam(value="num", required=false, defaultValue="1") Integer num  ) {
+		return id+" "+name+" "+num;
+	}
 }
